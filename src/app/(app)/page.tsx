26ee2d0a43178e3203f82c1c771e1f7d908a28bd
George Maia <georgemaiaf@gmail.com>
2025-09-22 13:38:37 -0300

    feat(landpage): :sparkles: Add Specialty

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,8 +1,11 @@
-import HeroSection from "@/components/hero-section";
+import { HeroSection, Specialty } from "@/components";
 
 export default function Home() {
   return (
-    <HeroSection />
+    <>
+      <HeroSection />
+      <Specialty />
+    </>
     // <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
     //   <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
     //     <Image
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from "./header";
 export * from "./hero-section";
 export * from "./logo";
+export * from "./specialty";
