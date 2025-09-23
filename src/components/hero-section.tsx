3e33fea5e5d2a7a0448ab89df88d65dad4af968b
George Maia <georgemaiaf@gmail.com>
2025-09-22 23:58:49 -0300

    feat(landpage): :speech_balloon: Atualiza conteúdo de Hero Section

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,156 +1,267 @@
-import React from "react";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
 import { HeroHeader } from "./header";
-import { InfiniteSlider } from "@/components/ui/infinite-slider";
-import { ProgressiveBlur } from "@/components/ui/progressive-blur";
+import {
+  Badge,
+  Button,
+  InfiniteSlider,
+  ProgressiveBlur,
+} from "@/components/ui";
+import { TextEffect } from "@/components/ui/text-effect";
+import { AnimatedGroup } from "@/components/ui/animated-group";
+
+const transitionVariants = {
+  item: {
+    hidden: {
+      opacity: 0,
+      filter: "blur(12px)",
+      y: 12,
+    },
+    visible: {
+      opacity: 1,
+      filter: "blur(0px)",
+      y: 0,
+      transition: {
+        type: "spring",
+        bounce: 0.3,
+        duration: 1.5,
+      },
+    },
+  },
+};
 
 export const HeroSection = () => {
   return (
     <>
       <HeroHeader />
-      <main className="overflow-x-hidden">
+      <main className="overflow-hidden">
+        <div
+          aria-hidden
+          className="absolute inset-0 isolate hidden opacity-65 contain-strict lg:block"
+        >
+          <div className="absolute top-0 left-0 h-320 w-140 -translate-y-87.5 -rotate-45 rounded-full bg-[radial-gradient(68.54%_68.72%_at_55.02%_31.46%,hsla(0,0%,85%,.08)_0,hsla(0,0%,55%,.02)_50%,hsla(0,0%,45%,0)_80%)]" />
+          <div className="absolute top-0 left-0 h-320 w-60 [translate:5%_-50%] -rotate-45 rounded-full bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.06)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)]" />
+          <div className="absolute top-0 left-0 h-320 w-60 -translate-y-87.5 -rotate-45 bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.04)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)]" />
+        </div>
         <section>
-          <div className="pt-12 pb-24 md:pb-32 lg:pt-44 lg:pb-56">
-            <div className="relative mx-auto flex max-w-6xl flex-col px-6 lg:block">
-              <div className="mx-auto max-w-lg text-center lg:ml-0 lg:w-1/2 lg:text-left">
-                <h1 className="mt-8 max-w-2xl text-5xl font-medium text-balance md:text-6xl lg:mt-16 xl:text-7xl">
-                  Ship 10x Faster with NS
-                </h1>
-                <p className="mt-8 max-w-2xl text-lg text-pretty">
-                  Highly customizable components for building modern websites
-                  and applications that look and feel the way you mean it.
-                </p>
+          <div className="relative pt-24 md:pt-36">
+            <AnimatedGroup
+              variants={{
+                container: {
+                  visible: {
+                    transition: {
+                      delayChildren: 1,
+                    },
+                  },
+                },
+                item: {
+                  hidden: {
+                    opacity: 0,
+                    y: 20,
+                  },
+                  visible: {
+                    opacity: 1,
+                    y: 0,
+                    transition: {
+                      type: "spring",
+                      bounce: 0.3,
+                      duration: 2,
+                    },
+                  },
+                },
+              }}
+              className="absolute inset-0 top-56 -z-20 mask-b-from-35% mask-b-to-90% lg:top-32"
+            >
+              <Image
+                src="https://ik.imagekit.io/lrigu76hy/tailark/night-background.jpg?updatedAt=1745733451120"
+                alt="background"
+                className="hidden size-full dark:block"
+                width="3276"
+                height="4095"
+              />
+            </AnimatedGroup>
 
-                <div className="mt-12 flex flex-col items-center justify-center gap-2 sm:flex-row lg:justify-start">
-                  <Button asChild size="lg" className="px-5 text-base">
-                    <Link href="#link">
-                      <span className="text-nowrap">Start Building</span>
-                    </Link>
-                  </Button>
+            <div
+              aria-hidden
+              className="absolute inset-0 -z-10 size-full [background:radial-gradient(125%_125%_at_50%_100%,transparent_0%,var(--color-background)_75%)]"
+            />
+
+            <div className="mx-auto max-w-7xl px-6">
+              <div className="text-center sm:mx-auto lg:mt-0 lg:mr-auto">
+                <AnimatedGroup
+                  variants={{
+                    container: {
+                      visible: {
+                        transition: {
+                          staggerChildren: 0.05,
+                          delayChildren: 0.175,
+                        },
+                      },
+                    },
+                    ...transitionVariants,
+                  }}
+                  className="mt-6"
+                >
+                  <Badge variant="outline">Desenvolvedor Full-Stack</Badge>
+                </AnimatedGroup>
+                <TextEffect
+                  preset="fade-in-blur"
+                  speedSegment={0.3}
+                  as="h1"
+                  className="mx-auto mt-4 max-w-4xl text-5xl text-balance max-md:font-semibold md:text-7xl lg:mt-6 xl:text-[5.25rem]"
+                >
+                  Oi, George Maia aqui!
+                </TextEffect>
+                <TextEffect
+                  per="line"
+                  preset="fade-in-blur"
+                  speedSegment={0.3}
+                  delay={0.5}
+                  as="p"
+                  className="mx-auto mt-8 max-w-2xl text-lg text-balance"
+                >
+                  Transformo ideias em soluções digitais completas. Ajudo
+                  startups e empresas a crescerem mais rápido com
+                  desenvolvimento web moderno e eficiente.
+                </TextEffect>
+
+                <AnimatedGroup
+                  variants={{
+                    container: {
+                      visible: {
+                        transition: {
+                          staggerChildren: 0.05,
+                          delayChildren: 0.75,
+                        },
+                      },
+                    },
+                    ...transitionVariants,
+                  }}
+                  className="mt-12 flex flex-col items-center justify-center gap-2 md:flex-row"
+                >
+                  <div
+                    key={1}
+                    className="bg-foreground/10 rounded-[calc(var(--radius-xl)+0.125rem)] border p-0.5"
+                  >
+                    <Button
+                      asChild
+                      size="lg"
+                      className="rounded-xl px-5 text-base"
+                    >
+                      <Link href="#contato">
+                        <span className="text-nowrap">Vamos conversar</span>
+                      </Link>
+                    </Button>
+                  </div>
                   <Button
                     key={2}
                     asChild
                     size="lg"
                     variant="ghost"
-                    className="px-5 text-base"
+                    className="h-10.5 rounded-xl px-5"
                   >
-                    <Link href="#link">
-                      <span className="text-nowrap">Request a demo</span>
+                    <Link href="#projetos">
+                      <span className="text-nowrap">Ver meus projetos</span>
                     </Link>
                   </Button>
-                </div>
+                </AnimatedGroup>
               </div>
-              <Image
-                className="-z-10 order-first ml-auto h-56 w-full object-cover invert sm:h-96 lg:absolute lg:inset-0 lg:-top-96 lg:-right-20 lg:order-last lg:h-max lg:w-2/3 lg:object-contain dark:mix-blend-lighten dark:invert-0"
-                src="https://ik.imagekit.io/lrigu76hy/tailark/abstract-bg.jpg?updatedAt=1745733473768"
-                alt="Abstract Object"
-                height="4000"
-                width="3000"
-              />
             </div>
           </div>
         </section>
-        <section className="bg-background pb-16 md:pb-32">
-          <div className="group relative m-auto max-w-6xl px-6">
-            <div className="flex flex-col items-center md:flex-row">
-              <div className="md:max-w-44 md:border-r md:pr-6">
-                <p className="text-end text-sm">Powering the best teams</p>
-              </div>
-              <div className="relative py-6 md:w-[calc(100%-11rem)]">
-                <InfiniteSlider speedOnHover={20} speed={40} gap={112}>
-                  <div className="flex">
-                    <Image
-                      className="mx-auto h-5 w-fit dark:invert"
-                      src="https://html.tailus.io/blocks/customers/nvidia.svg"
-                      alt="Nvidia Logo"
-                      height="2000"
-                      width="2000"
-                    />
-                  </div>
+        <section className="bg-background mt-12 pb-16 md:pb-32">
+          <AnimatedGroup
+            variants={{
+              container: {
+                visible: {
+                  transition: {
+                    staggerChildren: 0.05,
+                    delayChildren: 0.75,
+                  },
+                },
+              },
+              ...transitionVariants,
+            }}
+          >
+            <div className="group relative m-auto max-w-6xl px-6">
+              <div className="flex flex-col items-center md:flex-row">
+                <div className="md:max-w-44 md:border-r md:pr-6">
+                  <p className="text-end text-sm">Onde já fiz a diferença</p>
+                </div>
+                <div className="relative py-6 md:w-[calc(100%-11rem)]">
+                  <InfiniteSlider speedOnHover={20} speed={40} gap={112}>
+                    <Link
+                      href="https://www.konstroi.com/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <Image
+                        className="mx-auto h-4 w-fit"
+                        src="/logo/konstroi.png"
+                        alt="Column Logo"
+                        height="2000"
+                        width="2000"
+                      />
+                    </Link>
+                    <Link
+                      href="https://staging-ong-gabriel.vercel.app/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <Image
+                        className="mx-auto h-4 w-fit"
+                        src="/logo/ong-gabriel.svg"
+                        alt="Column Logo"
+                        height="2000"
+                        width="2000"
+                      />
+                    </Link>
+                    <Link
+                      href="https://playdevs.com.br/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <Image
+                        className="mx-auto h-4 w-fit"
+                        src="/logo/playdevs.png"
+                        alt="Column Logo"
+                        height="2000"
+                        width="2000"
+                      />
+                    </Link>
+                    <Link
+                      href="https://github.com/LovelaceLines"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <Image
+                        className="mx-auto h-4 w-fit"
+                        src="/logo/lovelace-lines.svg"
+                        alt="Column Logo"
+                        height="2000"
+                        width="2000"
+                      />
+                    </Link>
+                  </InfiniteSlider>
 
-                  <div className="flex">
-                    <Image
-                      className="mx-auto h-4 w-fit dark:invert"
-                      src="https://html.tailus.io/blocks/customers/column.svg"
-                      alt="Column Logo"
-                      height="2000"
-                      width="2000"
-                    />
-                  </div>
-                  <div className="flex">
-                    <Image
-                      className="mx-auto h-4 w-fit dark:invert"
-                      src="https://html.tailus.io/blocks/customers/github.svg"
-                      alt="GitHub Logo"
-                      height="2000"
-                      width="2000"
-                    />
-                  </div>
-                  <div className="flex">
-                    <Image
-                      className="mx-auto h-5 w-fit dark:invert"
-                      src="https://html.tailus.io/blocks/customers/nike.svg"
-                      alt="Nike Logo"
-                      height="2000"
-                      width="2000"
-                    />
-                  </div>
-                  <div className="flex">
-                    <Image
-                      className="mx-auto h-5 w-fit dark:invert"
-                      src="https://html.tailus.io/blocks/customers/lemonsqueezy.svg"
-                      alt="Lemon Squeezy Logo"
-                      height="2000"
-                      width="2000"
-                    />
-                  </div>
-                  <div className="flex">
-                    <Image
-                      className="mx-auto h-4 w-fit dark:invert"
-                      src="https://html.tailus.io/blocks/customers/laravel.svg"
-                      alt="Laravel Logo"
-                      height="2000"
-                      width="2000"
-                    />
-                  </div>
-                  <div className="flex">
-                    <Image
-                      className="mx-auto h-7 w-fit dark:invert"
-                      src="https://html.tailus.io/blocks/customers/lilly.svg"
-                      alt="Lilly Logo"
-                      height="2000"
-                      width="2000"
-                    />
-                  </div>
-
-                  <div className="flex">
-                    <Image
-                      className="mx-auto h-6 w-fit dark:invert"
-                      src="https://html.tailus.io/blocks/customers/openai.svg"
-                      alt="OpenAI Logo"
-                      height="2000"
-                      width="2000"
-                    />
-                  </div>
-                </InfiniteSlider>
-
-                <div className="from-background absolute inset-y-0 left-0 w-20 bg-linear-to-r"></div>
-                <div className="from-background absolute inset-y-0 right-0 w-20 bg-linear-to-l"></div>
-                <ProgressiveBlur
-                  className="pointer-events-none absolute top-0 left-0 h-full w-20"
-                  direction="left"
-                  blurIntensity={1}
-                />
-                <ProgressiveBlur
-                  className="pointer-events-none absolute top-0 right-0 h-full w-20"
-                  direction="right"
-                  blurIntensity={1}
-                />
+                  <div className="from-background absolute inset-y-0 left-0 w-20 bg-linear-to-r"></div>
+                  <div className="from-background absolute inset-y-0 right-0 w-20 bg-linear-to-l"></div>
+                  <ProgressiveBlur
+                    className="pointer-events-none absolute top-0 left-0 h-full w-20"
+                    direction="left"
+                    blurIntensity={1}
+                  />
+                  <ProgressiveBlur
+                    className="pointer-events-none absolute top-0 right-0 h-full w-20"
+                    direction="right"
+                    blurIntensity={1}
+                  />
+                </div>
               </div>
             </div>
-          </div>
+          </AnimatedGroup>
         </section>
       </main>
     </>
